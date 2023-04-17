bf04a5bed9706bb9b9a2297f94e2007d4119f622
your-name <katongole.roy100@gmail.com>
2023-04-17 23:14:38 +0300

    projects

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import Main from '@/components/Main'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
+//import Projects from '@/components/Projects'
 
 
 
