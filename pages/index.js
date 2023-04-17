1b116e23baf4a132a05405dbae638bc023f541cd
your-name <katongole.roy100@gmail.com>
2023-04-17 23:54:28 +0300

    projects

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import Main from '@/components/Main'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
+import Contact from '@/components/Contact'
 //import Projects from '@/components/Projects'
 
 
@@ -21,6 +22,7 @@ export default function Home() {
     <Main />
     <About  />
     <Skills />
+    <Contact />
 
       
 
