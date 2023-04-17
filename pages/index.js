d00ee6501dcf7b404f71b893c9ca4e8865684ea4
your-name <katongole.roy100@gmail.com>
2023-04-17 22:30:05 +0300

    about

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Main from '@/components/Main'
 
 
 
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Navbar />
+    <Main />
 
       
         
